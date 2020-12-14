106f44db5e40172b9f9db88ecfe66e32bf7c8f33
Diogo <souzanoris@gmail.com>
2020-12-13 23:56:56 -0300

    Add nodemon ao projeto.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,44 @@
 const express = require('express');
 
 const app = express();
+const port = 3333;
 
-app.get('/projects', (request, response) => {
-    return response.send('Hello Word');
+
+app.get('/', (request, response) => {
+    return response.json({ message: 'Hello Word'});
 });
 
-app.listen(3333);
+app.get('/projects', (request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2'
+    ]);
+});
+
+app.post('/projects', (request, response) => {
+    return response.json([
+        'Projeto 1',
+        'Projeto 2',
+        'Projeto 3'
+    ]);
+});
+
+app.put('/projects/:id', (request, response) => {
+
+    return response.json([
+        'Projeto 4',
+        'Projeto 2',
+        'Projeto 3'
+    ]);
+});
+
+app.delete('/projects/:id', (request, response) => {
+    return response.json([
+        'Projeto 2',
+        'Projeto 3'
+    ]);
+});
+
+app.listen(port, () => {
+    console.log(`🙌 Server running ${port}`);
+});
